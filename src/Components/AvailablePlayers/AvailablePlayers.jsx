1290d9b31e90a8a3ca76c248be83ef5a979f9aaa
hellosiyam <hellosiyambd@gmail.com>
2025-02-11 16:19:18 +0600

    assignment7 Done

--- src/Components/AvailablePlayers/AvailablePlayers.jsx
+++ src/Components/AvailablePlayers/AvailablePlayers.jsx
@@ -15,9 +15,9 @@ const AvailablePlayers = ({ buyHandel, setSelected, selectPlayers, decreesLength
         <div className="lg:max-w-7xl lg:mx-auto py-6 px-4 lg:px-0 lg:py-16 flex flex-col gap-8">
             <div className="flex flex-col lg:flex-row justify-between gap-2.5 lg:gap-0">
                 <h2 className="text-xl lg:text-3xl text-[#131313] font-semibold lg:font-bold items-center">Available Players : {players.length - decreesLength}</h2>
-                <div className="border-2 border-[#1313131A] rounded-2xl">
-                    <button className="py-[14px] px-[30px] rounded-l-2xl text-white hover:bg-white hover:text-black bg-blue-500 text-base font-normal cursor-pointer" type="button">Available</button>
-                    <button className="py-[14px] px-[30px] rounded-r-2xl text-[#13131399] font-normal text-base hover:bg-blue-500 hover:text-white cursor-pointer" type="button" onClick={() => setSelected(true)}>Selected ({selectPlayers.length})</button>
+                <div>
+                    <button className="py-[14px] px-[30px] rounded-l-2xl text-white hover:bg-white hover:text-black bg-blue-500 text-base font-normal cursor-pointer border-t border-l border-b border-[#1313131A]" type="button">Available</button>
+                    <button className="py-[14px] px-[30px] rounded-r-2xl text-[#13131399] font-normal text-base hover:bg-blue-500 hover:text-white cursor-pointer border-t border-r border-b border-[#1313131A]" type="button" onClick={() => setSelected(true)}>Selected ({selectPlayers.length})</button>
                 </div>
             </div>
             <div className="grid lg:grid-cols-3 gap-[24px]">
--- src/Components/SelectedPlayers/SelectedPlayers.jsx
+++ src/Components/SelectedPlayers/SelectedPlayers.jsx
@@ -6,9 +6,9 @@ const SelectedPlayers = ({ selectedHandel, setSelected, selectPlayers, deleteHan
         <div className="lg:max-w-7xl lg:mx-auto py-6 px-4 lg:px-0 lg:py-16 flex flex-col gap-8">
             <div className="flex flex-col lg:flex-row justify-between gap-2.5 lg:gap-0">
                 <h2 className="text-xl lg:text-3xl text-[#131313] font-semibold lg:font-bold items-center">Selected Player ({selectPlayers.length}/6)</h2>
-                <div className="border-2 border-[#1313131A] rounded-2xl">
-                    <button className="py-[14px] px-[30px] rounded-l-2xl text-[#13131399] font-normal text-base hover:bg-blue-500 hover:text-white cursor-pointer" type="button" onClick={() => setSelected(false)}>Available</button>
-                    <button onClick={selectedHandel} className="py-[14px] px-[30px] rounded-r-2xl text-white hover:bg-white hover:text-black bg-blue-500 text-base font-normal cursor-pointer" type="button">Selected ({selectPlayers.length})</button>
+                <div className="">
+                    <button className="py-[14px] px-[30px] rounded-l-2xl text-[#13131399] font-normal text-base hover:bg-blue-500 hover:text-white cursor-pointer border-t border-l border-b border-[#1313131A]" type="button" onClick={() => setSelected(false)}>Available</button>
+                    <button onClick={selectedHandel} className="py-[14px] px-[30px] rounded-r-2xl text-white hover:bg-white hover:text-black bg-blue-500 text-base font-normal cursor-pointer border-t border-r border-b border-[#1313131A]" type="button">Selected ({selectPlayers.length})</button>
                 </div>
             </div>
             <div className='flex flex-col gap-4'>
